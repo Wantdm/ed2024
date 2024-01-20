dbb10137194ddcd8f7ed395b2b0bf78c10855e9f
Wantdm <stanleyyangperson@gmail.com>
2024-01-19 23:47:27 -0500

    a

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,45 @@
 const CACHE_NAME = 'site-static-v1';
 const assets = [
   '/',
-  '/index.html',
+  '/html/index.html',
   '/css/style.css',
-  '/js/main.js',
-  
+  '/javascript/script.js'
 ];
 
 // Install event
 self.addEventListener('install', evt => {
   evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('caching shell assets');
-      cache.addAll(assets);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Caching shell assets');
+        return cache.addAll(assets);
+      })
   );
 });
 
 // Activate event
 self.addEventListener('activate', evt => {
-  // code to remove old caches
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      // Remove old caches
+      return Promise.all(keys
+        .filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
 });
 
 // Fetch event
 self.addEventListener('fetch', evt => {
   evt.respondWith(
     caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request);
+      return cacheRes || fetch(evt.request).catch(() => {
+        // Fallback page, e.g., for offline use
+        if (evt.request.mode === 'navigate') {
+          return caches.match('/offline.html');
+        }
+      });
     })
   );
 });
